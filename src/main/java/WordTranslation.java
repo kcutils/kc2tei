2b60805d9452d9b4116f04860cbcf307b5092e29
S. Fischer <sfis@informatik.uni-kiel.de>
2017-08-28 10:11:08 +0200

    Grammatik verfeinert: nicht alle Verzoegerungen sind auch lautlich repraesentiert; Laengungen koennen an Wortraendern und beliebig oft in Worten auftreten

--- src/main/java/WordTranslation.java
+++ src/main/java/WordTranslation.java
@@ -1,7 +1,5 @@
 import transliteration.analysis.DepthFirstAdapter;
-import transliteration.node.Node;
-import transliteration.node.THesistation;
-import transliteration.node.TWord;
+import transliteration.node.*;
 
 /*
  * class to collect words from transliteration section
@@ -14,12 +12,25 @@ public class WordTranslation extends DepthFirstAdapter {
   public WordTranslation(AnnotationElementCollection annotationElementCollection) {
     this.annotationElementCollection = annotationElementCollection;
   }
-
+/*
   public void caseTWord (TWord node) {
     annotationElementCollection.add(new Word((Node) node));
   }
 
-  public void caseTHesistation (THesistation node) {
+  public void caseTHesistWord (THesistWord node) {
+    annotationElementCollection.add(new Word((Node) node));
+  }
+
+  public void caseAHesistWordTransliterationContent (AHesistWordTransliterationContent node) {
+    annotationElementCollection.add(new Word((Node) node));
+  }
+
+  */
+  public void caseAWordTransliterationContent (AWordTransliterationContent node) {
+    annotationElementCollection.add(new Word((Node) node));
+  }
+
+  public void caseAHesistationTransliterationContent (AHesistationTransliterationContent node) {
     annotationElementCollection.add(new Word((Node) node));
   }
 }
