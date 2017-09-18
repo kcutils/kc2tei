14ebe32981ea6602928b06529a68bd488f829a67
S. Fischer <sfis@informatik.uni-kiel.de>
2017-09-18 09:51:35 +0200

    Wrapper-Skript: Fehlerbehandlungen hinzugefuegt

--- kc2tei.sh
+++ kc2tei.sh
@@ -2,5 +2,17 @@
 
 JAVA=$( which java )
 
-$JAVA -jar target/kc2tei-0.1.jar $@
+if [ $? -ne 0 ]; then
+  echo "Java not found in PATH! Exiting ..."
+  exit 1
+fi
+
+JAR="target/kc2tei-0.1.jar"
+
+if [ ! -f "$JAR" ]; then
+  echo "kc2tei JAR file not found! Exiting ..."
+  exit 1
+fi
+
+$JAVA -jar "$JAR" $@
 
